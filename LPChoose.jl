bc4be9594b4a929a3d8d9ad2a546438f4934af71
qtlchengadmin <reworkhow@gmail.com>
2020-08-06 22:51:03 -0700

    update notations

--- LPChoose.jl
+++ LPChoose.jl
@@ -40,8 +40,8 @@ function LPChoose(hapblock,budget="unlimited",MAF=0.0;
                   weights_for_haplotypes = "haplotype frequency", #"equal", "haplotype frequency", "rare haplotype preferred",
                   sequencing_homozygous_haplotypes_only = false)
     #Get incidence matrix
-    A01_all,freq_all, animals_all = convert_input_to_A(hapblock,MAF,sequencing_homozygous_haplotypes_only)
-    A01,freq, animals = select_these_animals(A01_all,freq_all,animals_all,preselected_animals)
+    A_all,freq_all, animals_all = convert_input_to_A(hapblock,MAF,sequencing_homozygous_haplotypes_only)
+    A,freq, animals = select_these_animals(A_all,freq_all,animals_all,preselected_animals)
     nind     = length(animals)
     #User-defined weights for haplotypes used in application 2
     if weights_for_haplotypes == "haplotype frequency"
@@ -63,7 +63,7 @@ function LPChoose(hapblock,budget="unlimited",MAF=0.0;
         model = Model(GLPK.Optimizer)
         @variable(model, select_this_animal[1:nind],Bin)
         @objective(model, Min, sum([importance[i]*select_this_animal[i] for i= 1:nind]))
-        @constraint(model, A01 * select_this_animal .>= 1)
+        @constraint(model, A * select_this_animal .>= 1)
         print("It took")
         @time optimize!(model)
         if JuMP.has_values(model)
@@ -86,8 +86,8 @@ function LPChoose(hapblock,budget="unlimited",MAF=0.0;
         nsteps           = (budget%budget_each_step != 0) ? div(budget,budget_each_step)+1 : div(budget,budget_each_step)
 
         animals_now      = copy(animals)
-        A01now           = copy(A01)
-        importance       = Matrix(weights_for_haplotypes'A01now)#get importance of each individual
+        Anow           = copy(A)
+        importance       = Matrix(weights_for_haplotypes'Anow)#get importance of each individual
 
         #Create text files to save output at each step
         file_genome_coverage    =open("genome_coverage.txt","w")
@@ -96,8 +96,8 @@ function LPChoose(hapblock,budget="unlimited",MAF=0.0;
         writedlm(file_genome_coverage,["step" "genome_covereage"],',')
         writedlm(file_hap_coverage,["step" "haplotype_coverage"],',')
         writedlm(file_identified_animals, ["step" "ID"],',')
-        writedlm(file_genome_coverage,["0" 1-sum(A01now)/sum(A01_all)],      ',')
-        writedlm(file_hap_coverage,   ["0" 1-size(A01now,1)/size(A01_all,1)],',')
+        writedlm(file_genome_coverage,["0" 1-sum(Anow)/sum(A_all)],      ',')
+        writedlm(file_hap_coverage,   ["0" 1-size(Anow,1)/size(A_all,1)],',')
         if preselected_animals != false
             writedlm(file_identified_animals,[fill("0",length(preselected_animals)) preselected_animals],',')
         else
@@ -112,7 +112,7 @@ function LPChoose(hapblock,budget="unlimited",MAF=0.0;
             model = Model(GLPK.Optimizer)
             @variable(model, select_this_animal[1:nind],Bin)
             @objective(model, Max, sum([importance[i]*select_this_animal[i] for i= 1:nind]))
-            @constraint(model, A01now * select_this_animal .<= 2)
+            @constraint(model, Anow * select_this_animal .<= 2)
             @constraint(model, sum(select_this_animal) <= budget_each_step) #e.g.,selecte 2 animals at each step
             #print("Step $stepi took") @time optimize!(model)
             optimize!(model)
@@ -123,25 +123,25 @@ function LPChoose(hapblock,budget="unlimited",MAF=0.0;
             select_this_animal = (JuMP.value.(select_this_animal) .== 1.0) #boolean
             writedlm(file_identified_animals,[fill(string(stepi),sum(select_this_animal)) animals_now[select_this_animal]],',')
             #get identified haplotypes at this step
-            haps_identified    = (vec(sum(A01now[:,select_this_animal],dims=2)) .!= 0) #boolean
+            haps_identified    = (vec(sum(Anow[:,select_this_animal],dims=2)) .!= 0) #boolean
             #update to current remaining animals for next round of selection
-            A01now             = A01now[.!haps_identified,.!select_this_animal]
+            Anow             = Anow[.!haps_identified,.!select_this_animal]
 
             weights_for_haplotypes  = weights_for_haplotypes[.!haps_identified]
-            importance              = Matrix(weights_for_haplotypes'A01now)#get importance of each individual
+            importance              = Matrix(weights_for_haplotypes'Anow)#get importance of each individual
             animals_now             = animals_now[.!select_this_animal]
             #save output to text files at this step
-            writedlm(file_genome_coverage,[string(stepi) 1-sum(A01now)/sum(A01_all)],',')
-            writedlm(file_hap_coverage,[string(stepi) 1-size(A01now,1)/size(A01_all,1)],',')
+            writedlm(file_genome_coverage,[string(stepi) 1-sum(Anow)/sum(A_all)],',')
+            writedlm(file_hap_coverage,[string(stepi) 1-size(Anow,1)/size(A_all,1)],',')
         end
         close(file_genome_coverage)
         close(file_hap_coverage)
         close(file_identified_animals)
-        println("\n",1 - size(A01now,1)/size(A01_all,1), " of the unique haplotypes in the population is covered.")
-        println(1 - sum(A01now)/sum(A01_all), " of the genome in the population is covered.\n")
+        println("\n",1 - size(Anow,1)/size(A_all,1), " of the unique haplotypes in the population is covered.")
+        println(1 - sum(Anow)/sum(A_all), " of the genome in the population is covered.\n")
         if preselected_animals != false
-            println("\n",1 - size(A01now,1)/size(A01,1), " of the unique haplotypes in the population (exclude preselected animals) is covered.")
-            println(1 - sum(A01now)/sum(A01), " of the genome in the population (exclude preselected animals) is covered.\n")
+            println("\n",1 - size(Anow,1)/size(A,1), " of the unique haplotypes in the population (exclude preselected animals) is covered.")
+            println(1 - sum(Anow)/sum(A), " of the genome in the population (exclude preselected animals) is covered.\n")
         end
         #writedlm("identified_animals.txt",setdiff(animals,animals_now))
         println("IDs for identified animals were saved in identified_animals.txt.\n")
@@ -284,24 +284,24 @@ end
 """
 function check_genome_coverage(hapblock,preselected_animals,MAF=0.0)
 
-    A01,freq, animals = convert_input_to_A(hapblock,MAF)
+    A,freq, animals = convert_input_to_A(hapblock,MAF)
 
     println("\n\n\n\n\n")
     println("--------------CHECK GENOME COVERAGE---------------")
     println("-------------by preselected animals---------------")
 
-    A01_remaining, freq_remaining, animals_remaining = select_these_animals(A01,freq, animals,preselected_animals)
-    println("\n",1 - size(A01_remaining,1)/size(A01,1), " of the unique haplotypes in the population is covered.")
-    println(1 - sum(A01_remaining)/sum(A01), " of the genome in the population is covered.\n")
+    A_remaining, freq_remaining, animals_remaining = select_these_animals(A,freq, animals,preselected_animals)
+    println("\n",1 - size(A_remaining,1)/size(A,1), " of the unique haplotypes in the population is covered.")
+    println(1 - sum(A_remaining)/sum(A), " of the genome in the population is covered.\n")
 
     println("------------Remaining Animals-----------------------")
     println("#Animal:",length(animals))
     println("Haplotype Frequency (remaining animals) ",summarystats(freq_remaining))
 end
 
-function select_these_animals(A01,freq,animals,preselected_animals)
+function select_these_animals(A,freq,animals,preselected_animals)
     if preselected_animals == false
-        return A01,freq,animals
+        return A,freq,animals
     elseif typeof(preselected_animals) == String
         preselected_animals = readdlm(preselected_animals,Int64)
     else
@@ -311,9 +311,9 @@ function select_these_animals(A01,freq,animals,preselected_animals)
         error("Selected animals IDs are not found!")
     end
     select_this_animal = [i in preselected_animals for i in animals] #boolean
-    haps_identified    = (vec(sum(A01[:,select_this_animal],dims=2)) .!= 0) #boolean
-    A01_remaining      = A01[.!haps_identified,.!select_this_animal]
+    haps_identified    = (vec(sum(A[:,select_this_animal],dims=2)) .!= 0) #boolean
+    A_remaining      = A[.!haps_identified,.!select_this_animal]
     animals_remaining  = animals[.!select_this_animal]
     freq_remaining     = freq[.!haps_identified]
-    return A01_remaining, freq_remaining, animals_remaining
+    return A_remaining, freq_remaining, animals_remaining
 end
